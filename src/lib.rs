6a33890fa07f7bfcc407b6f2553675295f61222f
frithjofhoppebit <frithjof.hoppe@bit.admin.ch>
2024-05-23 13:26:49 +0200

    test: :test_tube: Add short test for key generation

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,14 @@ mod test {
     use crate::trustdidweb::Base64MultiBaseConverter;
 
     use super::trustdidweb::{Ed25519KeyPair, Ed25519SigningKey, Ed25519VerifyingKey};
-    use rstest::{rstest};
-    
+    use rstest::rstest;
+
     #[rstest]
     fn test_key_creation() {
         let key_pair = Ed25519KeyPair::generate();
         let original_private = key_pair.get_signing_key();
         let original_public = key_pair.get_verifying_key();
-        
+
         let new_private = Ed25519SigningKey::from_multibase(&original_private.to_multibase());
         let new_public = Ed25519VerifyingKey::from_multibase(&original_public.to_multibase());
 
