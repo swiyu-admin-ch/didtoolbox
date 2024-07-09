5dd1d17035964a940618dc8af8c2aaad933d06f6
frithjofhoppebit <frithjof.hoppe@bit.admin.ch>
2024-07-09 09:39:05 +0200

    ci: :green_heart: Add build for android lib file

--- kotlin-build.sh
+++ kotlin-build.sh
@@ -1,3 +1,3 @@
 cargo build --release
+cargo ndk -o ./bindings/kotlin/jniLibs --manifest-path ./Cargo.toml -t armeabi-v7a -t arm64-v8a -t x86 -t x86_64 build --release
 cargo run --bin uniffi-bindgen generate --library target/release/libdidtoolbox.so --language kotlin --out-dir bindings/kotlin
-cp target/release/libdidtoolbox.so bindings/kotlin
